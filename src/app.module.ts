e996740402245fbeb1dc362c53362e78370ee653
spykelionel <social.lionel@gmail.com>
2023-12-07 08:58:10 +0100

    add todo module to app

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats.module';
+import TodoModule from './todo/todo.module';
 
 @Module({
-  imports: [CatsModule],
+  imports: [CatsModule, TodoModule],
   controllers: [AppController],
   providers: [AppService],
 })
