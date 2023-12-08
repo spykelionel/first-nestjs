6944a2aabb24e7197547ab2d6637ec73d7863510
spykelionel <social.lionel@gmail.com>
2023-12-08 05:17:26 +0100

    handle paths to todo

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,11 +1,23 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import TodoService from './todo.service';
+import CreateTodoDTO from './dto/create-todo.dto';
+import { Todo } from './Todo';
 
 @Controller('todo')
 export default class TodoController {
   constructor(private readonly todoService: TodoService) {}
   @Get()
-  getAll() {
+  getAll(): Todo[] {
     return this.todoService.getAll();
+  }
+
+  @Get('id')
+  getSingleTodo(@Param('id') id: number): Todo {
+    return this.todoService.getSingleTodo(id);
+  }
+
+  @Post()
+  createTodo(@Body() createTodoDto: CreateTodoDTO) {
+    this.todoService.creatTodo(createTodoDto);
   }
 }
