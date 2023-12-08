65f0c236c09e8690047cfcf9b48cf3c2707edb20
spykelionel <social.lionel@gmail.com>
2023-12-08 05:17:09 +0100

    create todo methods

--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -1,8 +1,37 @@
 import { Injectable } from '@nestjs/common';
+import { Todo } from './Todo';
+import CreateTodoDTO from './dto/create-todo.dto';
 
+const init: Todo[] = [
+  {
+    title: 'A todo',
+    status: 'pending',
+    id: 1,
+  },
+  {
+    title: 'A todo',
+    status: 'pending',
+    id: 2,
+  },
+  {
+    title: 'A todo',
+    status: 'pending',
+    id: 3,
+  },
+];
 @Injectable()
 export default class TodoService {
-  getAll(): string {
-    return 'get all todos';
+  private readonly todos: Todo[] = init;
+  getAll(): Todo[] {
+    return this.todos;
+  }
+
+  creatTodo(createTodoDto: CreateTodoDTO) {
+    this.todos.push({ ...createTodoDto, id: this.todos.length + 1 });
+  }
+
+  getSingleTodo(id: number): Todo {
+    const todo = this.todos.find((t) => t.id === id);
+    return todo;
   }
 }
