527d13b17b83e54fc5e2939319ce840ec21c1eae
spykelionel <social.lionel@gmail.com>
2023-12-15 07:25:54 +0100

    add updateTodo controller

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
 import TodoService from './todo.service';
 import CreateTodoDTO from './dto/create-todo.dto';
 import { Todo } from './Todo';
+import { UpdateTodoDTO } from './dto/update-todo.dto';
 
 @Controller('todo')
 export default class TodoController {
@@ -17,7 +18,12 @@ export default class TodoController {
   }
 
   @Post()
-  createTodo(@Body() createTodoDto: CreateTodoDTO) {
+  createTodo(@Body() createTodoDto: CreateTodoDTO): Todo {
     return this.todoService.createTodo(createTodoDto);
+  }
+
+  @Put(':id')
+  updateTodo(@Param('id') id: number, payload: UpdateTodoDTO): Todo | String {
+    return this.todoService.updateTodo(id, payload);
   }
 }
