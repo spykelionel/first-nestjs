634e7c9e0d054e04bc98ba3d8e74a6cc43ac2cf0
spykelionel <social.lionel@gmail.com>
2023-12-15 08:26:56 +0100

    configure db

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,27 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats.module';
 import TodoModule from './todo/todo.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
+import { Todo } from './todo/todo.entity';
 
 @Module({
-  imports: [CatsModule, TodoModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'password',
+      database: 'todo',
+      entities: [Todo],
+      synchronize: true,
+    }),
+    TodoModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private readonly dataSource: DataSource) {}
+}
