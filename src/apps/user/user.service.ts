3f14dad2b39d88d71f5c84c89e28bfeb52bcb117
spykelionel <social.lionel@gmail.com>
2023-12-16 16:41:26 +0100

    create user service

---
+++ src/apps/user/user.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './data/user.entity';
+import { DeleteResult, InsertResult, Repository, UpdateResult } from 'typeorm';
+import { IUserDAO } from './data/user.dao';
+import { CreateUserDTO, UpdateUserDTO } from './data/user.dto';
+
+@Injectable()
+export class UserService implements IUserDAO {
+  constructor(
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
+  ) {}
+
+  createUser(createUserDto: CreateUserDTO): Promise<InsertResult> {
+    return this.userRepository.insert(createUserDto);
+  }
+
+  updateUser(id: number, updateUserDto: UpdateUserDTO): Promise<UpdateResult> {
+    return this.userRepository.update(id, updateUserDto);
+  }
+  getAllUsers(): Promise<User[]> {
+    return this.userRepository.find();
+  }
+  getSingleUser(id: number): Promise<User> {
+    return this.userRepository.findOneByOrFail({ id });
+  }
+  deleteSingleUser(id: number): Promise<DeleteResult> {
+    return this.userRepository.softDelete({ id });
+  }
+}
