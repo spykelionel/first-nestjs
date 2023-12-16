36b05d0799ff2a0e8c23fefd612fd12f393a04c2
spykelionel <social.lionel@gmail.com>
2023-12-16 15:42:15 +0100

    update return types

--- src/todo/dao/todo-dao.ts
+++ src/todo/dao/todo-dao.ts
@@ -1,3 +1,4 @@
+import { DeleteResult, InsertResult } from 'typeorm';
 import { Todo } from '../Todo';
 import CreateTodoDTO from '../dto/create-todo.dto';
 import { UpdateTodoDTO } from '../dto/update-todo.dto';
@@ -5,8 +6,8 @@ import { Todo as ETodo } from '../todo.entity';
 
 export interface ITodoDAO {
   getSingleTodo(id: number): Promise<ETodo> | undefined;
-  addTodo(todo: CreateTodoDTO);
-  deleteTodo(id: number);
+  addTodo(todo: CreateTodoDTO): Promise<InsertResult>;
+  deleteTodo(id: number): Promise<DeleteResult>;
   updateTodo(id: number, payload: UpdateTodoDTO): Todo | undefined | string;
   findAllTodos(): Promise<ETodo[]>;
 }
