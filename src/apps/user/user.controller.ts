526ec6442c9633fd8f083df54894abe4405ac148
spykelionel <social.lionel@gmail.com>
2023-12-16 16:41:42 +0100

    create user controller

---
+++ src/apps/user/user.controller.ts
@@ -0,0 +1,41 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { UserService } from './user.service';
+import { CreateUserDTO, UpdateUserDTO } from './data/user.dto';
+
+@Controller('user')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post()
+  createUser(@Body() createUserDto: CreateUserDTO) {
+    return this.userService.createUser(createUserDto);
+  }
+
+  @Get()
+  getAllUsers() {
+    return this.userService.getAllUsers();
+  }
+
+  @Get(':id')
+  getSingleUser(@Param('id') id: number) {
+    return this.userService.getSingleUser(id);
+  }
+
+  @Put(':id')
+  updateUser(@Param('id') id: number, @Body() updateUserDto: UpdateUserDTO) {
+    return this.userService.updateUser(+id, updateUserDto);
+  }
+
+  @Delete(':id')
+  deleteSingleUser(@Param('id') id: number) {
+    return this.userService.deleteSingleUser(id);
+  }
+}
